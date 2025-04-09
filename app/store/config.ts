f75b8196254e558b4b207b9eed9d2a282ec931da
yue25699 <54253932+yue25699@users.noreply.github.com>
2025-04-09 17:28:57 +0800

    Update config.ts

--- app/store/config.ts
+++ app/store/config.ts
@@ -64,7 +64,7 @@ export const DEFAULT_CONFIG = {
   models: DEFAULT_MODELS as any as LLMModel[],
 
   modelConfig: {
-    model: "gpt-4o-mini" as ModelType,
+    model: "gpt-4o-image" as ModelType,
     providerName: "OpenAI" as ServiceProvider,
     temperature: 0.5,
     top_p: 1,
