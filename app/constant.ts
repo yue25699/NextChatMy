d440a618e3c185af7fe3bb887a8202d3c7c01362
yue25699 <54253932+yue25699@users.noreply.github.com>
2025-04-10 11:47:40 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -476,6 +476,7 @@ export const EXCLUDE_VISION_MODEL_REGEXES = [/claude-3-5-haiku-20241022/];
 const openaiModels = [
   // As of July 2024, gpt-4o-mini should be used in place of gpt-3.5-turbo,
   // as it is cheaper, more capable, multimodal, and just as fast. gpt-3.5-turbo is still available for use in the API.
+  "gpt-4o-image",
   "gpt-3.5-turbo",
   "gpt-3.5-turbo-1106",
   "gpt-3.5-turbo-0125",
